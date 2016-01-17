d55fda3f604940dbf27b35702e0a0c1cadc92d9d
Patrick Breucking <me@patrickbreucking.de>
2016-01-17 20:31:03 +0100

    Added model class ManagedObject

---
+++ src/main/java/de/telekom/cot/client/model/ManagedObject.java
@@ -0,0 +1,25 @@
+package de.telekom.cot.client.model;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class ManagedObject {
+    private final  StringProperty name;
+    
+    public ManagedObject() {
+    	name = null;
+    }
+    
+    public ManagedObject(String name) {
+        this.name = new SimpleStringProperty(name);
+    }
+
+	public String getName() {
+		return name.get();
+	}
+
+	public void setName(String name) {
+		this.name.set(name);
+	}
+
+}
